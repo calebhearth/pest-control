2c353aff9627a830cb6e1d409d5da5a03e0d6a22
cjaysson <cjaysson@c1a93738-a89d-add3-e846-535381a29ce9>
2011-01-29 04:56:03 +0000

    updated includes, removed controller-specific game exiting.
    git-svn-id: http://pest-control.googlecode.com/svn/trunk@5 c1a93738-a89d-add3-e846-535381a29ce9

--- pest-control/pest-control/Game.cs
+++ pest-control/pest-control/Game.cs
@@ -2,12 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Audio;
 using Microsoft.Xna.Framework.Content;
-using Microsoft.Xna.Framework.GamerServices;
 using Microsoft.Xna.Framework.Graphics;
-using Microsoft.Xna.Framework.Input;
-using Microsoft.Xna.Framework.Media;
 
 namespace pest_control
 {
@@ -66,11 +62,6 @@ namespace pest_control
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Update(GameTime gameTime)
         {
-            // Allows the game to exit
-            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
-                this.Exit();
-
-            // TODO: Add your update logic here
 
             base.Update(gameTime);
         }
